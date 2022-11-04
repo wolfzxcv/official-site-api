0f3441741dac1d12e0017c19f02a9eee86fd4242
Alice Chou <alice1qaz@gmail.com>
2022-11-04 18:02:23 +0800

    fix(file size): modify file size, height of ckeditor

--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,8 @@ app.use(morganMiddleware);
 app.use(methodOverride('_method'));
 
 // Body parser middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: false }));
 
 app.use(
   session({
